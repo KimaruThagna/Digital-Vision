da86d4834a8f3b748f539dd1873ba94775d351b5
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-09 09:39:37 +0300

    fix glob regex pattern

--- detect_obj.py
+++ detect_obj.py
@@ -10,8 +10,8 @@ detection_obj = ObjectDetection()
 detection_obj.setModelTypeAsRetinaNet()# you can choose between retinaNet, YOLOv3 and TinyYOLOv3
 detection_obj.setModelPath( "models/resnet50_coco_best_v2.0.1.h5")
 detection_obj.loadModel()
-for img in glob("inputs"):
+for img in glob("inputs/*"):
     detections = detection_obj.detectObjectsFromImage(input_image= img,
-                                                      output_image_path=os.path.join("outputs/" , f'detected_{img}'))
+                                                      output_image_path=os.path.join("outputs/" , f'detected_{str(img).split("/")[1]}'))
     for eachObject in detections:
         print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
