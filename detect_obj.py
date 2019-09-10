94ccaeab5abc5428e721667454060bcdc236b1d8
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-10 06:36:47 +0300

    adjust minimum probability and allow image extraction

--- detect_obj.py
+++ detect_obj.py
@@ -11,7 +11,11 @@ detection_obj.setModelTypeAsRetinaNet()# you can choose between retinaNet, YOLOv
 detection_obj.setModelPath( "models/resnet50_coco_best_v2.0.1.h5")
 detection_obj.loadModel()
 for img in glob("inputs/*"):
-    detections = detection_obj.detectObjectsFromImage(input_image= img,
-                                                      output_image_path=os.path.join("outputs/" , f'detected_{str(img).split("/")[1]}'))
+    detections, extracted_images = detection_obj.detectObjectsFromImage(input_image= img,
+                                                      output_image_path=os.path.join("outputs/" , f'detected_{str(img).split("/")[1]}'),
+                                                                                     extract_detected_objects=True,
+                                                                        minimum_percentage_probability=80)
+    # the last parameter allows us to extract images retrieved by the bounding box as independent images
+    # adjust minimum probability to set a threshold where the output obeys.
     for eachObject in detections:
-        print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+        print(eachObject["name"] , " : " , eachObject["percentage_probability"] ) # retrieve the object name and relevant probability
