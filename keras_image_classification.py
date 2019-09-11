eb6f7c2b056d950d6bcc8bbb3ff88a6a8e521448
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-11 09:21:08 +0300

    retrieve label with best probability

--- keras_image_classification.py
+++ keras_image_classification.py
@@ -8,7 +8,7 @@ from keras.applications import vgg19,resnet50,inception_v3
 # load model and set weights
 vgg_19 = vgg19.VGG19(weights='imagenet')
 resnet = resnet50.ResNet50(weights='imagenet')
-inception = inception_v3.InceptionV3(weights='imagenet')
+#inception = inception_v3.InceptionV3(weights='imagenet')
 
 # load image and set desired size then convert to numpy array since it has width height and channel
 # load_img() loads an image in PIL format with width and height dimensions
@@ -21,16 +21,16 @@ input = np.expand_dims(loaded_img,axis=0)
 
 vgg_preprocess = vgg19.preprocess_input(input.copy())
 resnet_preprocess = resnet50.preprocess_input(input.copy())
-inception_preprocess = inception_v3.preprocess_input(input.copy())
+#inception_preprocess = inception_v3.preprocess_input(input.copy())
 # make predictions
 predictions_vgg19 = vgg_19.predict(vgg_preprocess)
 label_vgg19 = decode_predictions(predictions_vgg19)
-print (f'Vgg_19 prediction={label_vgg19}')
+print (f'Vgg_19 prediction={label_vgg19[0][0][1]}')
 
 predictions_resnet = resnet.predict(resnet_preprocess)
 label_resnet = decode_predictions(predictions_resnet)
-print (f'Resnent prediction={label_resnet}')
+print (f'Resnent prediction={label_resnet[0][0][1]}')
 
-predictions_inception = inception.predict(inception_preprocess)
-label_inception = decode_predictions(predictions_inception)
-print (f'Inception prediction={label_inception}')
+# predictions_inception = inception.predict(inception_preprocess)
+# label_inception = decode_predictions(predictions_inception)
+# print (f'Inception prediction={label_inception}')
