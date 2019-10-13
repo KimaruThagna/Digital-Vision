2494024194cae28b952794999d90578ac2bee062
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-13 10:09:57 +0300

    loop over results and display

--- tesseract.py
+++ tesseract.py
@@ -164,3 +164,24 @@ for (startX, startY, endX, endY) in boxes:
 
 # sort the results bounding box coordinates from top to bottom
 results = sorted(results, key=lambda r: r[0][1])
+
+# loop over the results
+for ((startX, startY, endX, endY), text) in results:
+    # display the text OCR'd by Tesseract
+    print("OCR TEXT")
+    print("========")
+    print("{}\n".format(text))
+
+    # strip out non-ASCII text so we can draw the text on the image
+    # using OpenCV, then draw the text and a bounding box surrounding
+    # the text region of the input image
+    text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+    output = orig.copy()
+    cv2.rectangle(output, (startX, startY), (endX, endY),
+                  (0, 0, 255), 2)
+    cv2.putText(output, text, (startX, startY - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+
+    # show the output image
+    cv2.imshow("Text Detection", output)
+    cv2.waitKey(0)
