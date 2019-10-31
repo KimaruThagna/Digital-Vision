fcb5908fc460d8fb8fee5022a62b5ae198df39ca
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-31 14:07:05 +0300

    detect number of faces

--- detect_face.py
+++ detect_face.py
@@ -15,11 +15,10 @@ while True:
 
     # Find all the faces in the current frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
-
     # Display the results
     for top, right, bottom, left in face_locations:
         # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)# green rectangle around where face is
 
     # Display the resulting image
     cv2.imshow('Video', frame)
@@ -28,6 +27,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+print(f'Number of faces detected : {len(face_locations)}')
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
