d6496ff0b03346585423e10cb78e2ffdde48835a
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-16 17:45:11 +0300

    import packages for object detection

--- imageai_image_prediction.py
+++ imageai_image_prediction.py
@@ -4,7 +4,7 @@ prediction_obj = ImagePrediction() # prediction class
 prediction_obj.setModelTypeAsResNet()
 prediction_obj.setModelPath("models/resnet50_weights_tf_dim_ordering_tf_kernels.h5")
 prediction_obj.loadModel()
-prediction_objs, percentage_probabilities = prediction_obj.predictImage("inputs/img_test.jpg", result_count=3)
+prediction_objs, percentage_probabilities = prediction_obj.predictImage("inputs/test.jpeg", result_count=3)
 # result count gives the number of items we wish to spot in the image. Most common case would be the top 3 items range is 1-1000
 for index in range(len(prediction_objs)):
     print(prediction_objs[index] , " : " , percentage_probabilities[index])
---
+++ obj_detection.py
@@ -0,0 +1,7 @@
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
