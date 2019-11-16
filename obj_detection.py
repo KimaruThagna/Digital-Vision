0ec00ab3ce7b267a912dd1ff3550bd0307b65ce3
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-16 18:00:39 +0300

    detect green object

--- obj_detection.py
+++ obj_detection.py
@@ -23,8 +23,34 @@ tracked_points = deque(maxlen=args["buffer"])
 
 # if a video path was not supplied, grab the reference to the webcam
 if not args.get("video", False):
-    vs = VideoStream(src=0).start()
+    vs = VideoStream(src=0).start() # start webcam
 else: # otherwise, grab a reference to the video file
     vs = cv2.VideoCapture(args["video"])
 # allow the camera or video file to warm up
 time.sleep(2.0)
+# infinite loop
+
+while True:
+    # grab the current frame
+    frame = vs.read()
+
+    # handle the frame from VideoCapture or VideoStream
+    frame = frame[1] if args.get("video", False) else frame
+
+    # if we are viewing a video and we did not grab a frame,
+    # then we have reached the end of the video
+    if frame is None:
+        break
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
