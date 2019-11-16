cc200f9bb8c16be273b1f44640ea7fbbb51ffdac
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-16 20:25:08 +0300

    detect green object

--- obj_detection.py
+++ obj_detection.py
@@ -31,26 +31,19 @@ time.sleep(2.0)
 # infinite loop
 
 while True:
-    # grab the current frame
-    frame = vs.read()
-
+    frame = vs.read() # grab the current frame
     # handle the frame from VideoCapture or VideoStream
     frame = frame[1] if args.get("video", False) else frame
-
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
+    # if we are viewing a video and we did not grab a frame, then we have reached the end of the video
     if frame is None:
         break
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    frame = imutils.resize(frame, width=600) # resize the frame,
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0) # blur frame
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert to HSV
 
     # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
+    # a series of dilations and erosions to remove any small blobs left in the mask
     mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    mask = cv2.erode(mask, None, iterations=2) # remove boundaries in region of interest hence make smaller
+    mask = cv2.dilate(mask, None, iterations=2) # opposite of erosion
