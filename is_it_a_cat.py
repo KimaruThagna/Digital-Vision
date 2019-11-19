009803769ac018bbb25b8e0d47fd43c26080318a
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-19 16:42:17 +0300

    load an preprocess image

--- is_it_a_cat.py
+++ is_it_a_cat.py
@@ -9,3 +9,10 @@ ap.add_argument("-c", "--cascade",
                 default="haarcascade_frontalcatface.xml",
                 help="path to cat detector haar cascade")
 args = vars(ap.parse_args())
+image = cv2.imread(args["image"]) # load image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert it to grayscale
+# load the cat detector Haar cascade, then detect cat faces in image
+detector = cv2.CascadeClassifier(args["cascade"])
+rects = detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=10, minSize=(75, 75))
+# minNeighbours- parameter controls the minimum number of detected bounding boxes
+# in a given area for the region to be considered a “cat face”.
