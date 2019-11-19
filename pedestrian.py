b749b368ac35e1fddbfeac0690a8410d5eb78de5
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-19 17:08:41 +0300

    pedestrian detection import libraries

---
+++ pedestrian.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True, help="path to images directory")
+args = vars(ap.parse_args())
