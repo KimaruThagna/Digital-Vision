d4025f0af42c1830e0d526d603aeca89ed6ed78c
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-19 16:47:30 +0300

    draw results on image

--- is_it_a_cat.py
+++ is_it_a_cat.py
@@ -9,10 +9,23 @@ ap.add_argument("-c", "--cascade",
                 default="haarcascade_frontalcatface.xml",
                 help="path to cat detector haar cascade")
 args = vars(ap.parse_args())
-image = cv2.imread(args["image"]) # load image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert it to grayscale
-# load the cat detector Haar cascade, then detect cat faces in image
+
+# load the input image and convert it to grayscale
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# load the cat detector Haar cascade, then detect cat faces
+# in the input image
 detector = cv2.CascadeClassifier(args["cascade"])
-rects = detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=10, minSize=(75, 75))
-# minNeighbours- parameter controls the minimum number of detected bounding boxes
-# in a given area for the region to be considered a “cat face”.
+rects = detector.detectMultiScale(gray, scaleFactor=1.3,
+                                  minNeighbors=10, minSize=(75, 75))
+
+
+# loop over the cat faces and draw a rectangle surrounding each
+for (i, (x, y, w, h)) in enumerate(rects):
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(image, f'Cat #{i+1}', (x, y - 10),  cv2.FONT_ITALIC, 0.55, (0, 255, 255), 2)
+
+# show the detected cat faces
+cv2.imshow("Black Panther Relatives", image)
+cv2.waitKey(0)
